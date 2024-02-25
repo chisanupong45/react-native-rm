40b7a61fcee36b4d27814bab76cec53414adf5d9
Chisanupong Pinthong <chisanupong.p@kkumail.com>
2024-02-25 13:11:55 +0700

    Add Navigation Tab

---
+++ navigation/tabs.js
@@ -0,0 +1,132 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "../screens/HomeScreen";
+import CameraScreen from "../screens/CameraScreen";
+import InsectsScreen from "../screens/InsectsScreen";
+
+const Tab = createBottomTabNavigator();
+
+const CustomTabBarButton = ({ children, onPress}) => (
+  <TouchableOpacity
+    style={{
+      top: -30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      ... styles.shadow
+    }}
+    onPress={onPress}
+    >
+      <View style={{
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        backgroundColor: '#e32f45'
+      }}>
+        {children}
+      </View>
+  </TouchableOpacity>
+);
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: "#ffffff",
+          borderRadius: 15,
+          height: 80,
+          ...styles.shadow,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0}}>
+              <Image
+                source={require("../assets/icons/home.png")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                HOME
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name="Camera" component={CameraScreen} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image 
+              source={require('../assets/icons/camera.png')}
+              resizeMode="contain"
+              style={{
+                width:30,
+                height: 30,
+                tintColor: '#fff'
+              }}
+            />
+          ),
+          tabBarButton: (props) => (
+            <CustomTabBarButton { ...props} />
+          )
+        }}  
+      />
+      <Tab.Screen name="Insects" component={InsectsScreen} 
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0}}>
+              <Image
+                source={require("../assets/icons/bee.png")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                Insects
+              </Text>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
+
+export default Tabs;
